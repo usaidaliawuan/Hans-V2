7e59c485388299627c57815193968c1a2cb9a3cf
usaidaliawuan <147395262+usaidaliawuan@users.noreply.github.com>
2024-09-09 22:37:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 //Instagram: unicorn_xeon13
 //Telegram: t.me/xeonbotinc
 //GitHub: @DGXeon
-//WhatsApp: +916909137213
+//WhatsApp: +923118915268
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 
 const {
